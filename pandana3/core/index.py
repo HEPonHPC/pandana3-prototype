d1f09e79b06240427d83f79016219360a6bbf269
Marc Paterno <paterno@fnal.gov>
2021-08-18 17:26:58 -0500

    Add comments

--- pandana3/core/index.py
+++ pandana3/core/index.py
@@ -8,20 +8,37 @@ from pandana3.core.grouping import Grouping
 
 
 class Index:
+    """ "Index is the base class for indices.
+
+    Each concrete index is either a SimpleIndex or a MultiIndex. Each type
+    of index may or may not be trivial.
+
+    An index is trivial if it has an entry for every row of its associated
+    table. In such cases it is often not necessary to actually read any
+    indexing information from the file.
+    """
+
     def __init__(self, trivial: bool = True, grouping: Grouping = Grouping()):
         self.is_trivial = trivial
         self.gping = grouping
 
     def grouping(self) -> Grouping:
+        """Return the grouping level associated with this index."""
         return self.gping
 
 
 class SimpleIndex(Index):
+    """A SimpleIndex represents an index that contains row numbers, not values from
+    actual data columns.
+    """
+
     def __init__(self, trivial: bool = True):
         super(SimpleIndex, self).__init__(trivial)
 
 
 class MultiIndex(Index):
+    """A MultiIndex represents and index that contains data from columns."""
+
     def __init__(self, trivial: bool = True, grouping: Grouping = Grouping()):
         super(MultiIndex, self).__init__(trivial, grouping)
 
