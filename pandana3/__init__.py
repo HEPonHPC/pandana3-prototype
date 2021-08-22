d70ae039a1ef5abac7ac7ae456ad87a09a0dd3fe
Marc Paterno <paterno@fnal.gov>
2021-08-22 14:58:01 -0500

    Add docstring to top-level module

--- pandana3/__init__.py
+++ pandana3/__init__.py
@@ -1,3 +1,8 @@
+"""
+Provides facilities for performing analysis on hierarchical columnar data read from
+HDF5 files.
+"""
+
 from os.path import dirname, abspath
 
 ROOT_DIR = dirname(abspath(__file__))
