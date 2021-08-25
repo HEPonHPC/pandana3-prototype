5983e3ca7293474cd5e2aaffbfb15037f91d3110
Marc Paterno <paterno@fnal.gov>
2021-08-25 16:23:12 -0500

    Fix a variety of complains from pytlint

--- pandana3/core/index.py
+++ pandana3/core/index.py
@@ -33,7 +33,7 @@ class SimpleIndex(Index):
     """
 
     def __init__(self, trivial: bool = True):
-        super(SimpleIndex, self).__init__(trivial)
+        super().__init__(trivial)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, SimpleIndex):
@@ -45,10 +45,12 @@ class MultiIndex(Index):
     """A MultiIndex represents and index that contains data from columns."""
 
     def __init__(self, trivial: bool = True, grouping: Grouping = Grouping()):
-        super(MultiIndex, self).__init__(trivial, grouping)
+        super().__init__(trivial, grouping)
 
 
 def make_index(idx1: Index, idx2: Index) -> Index:
+    """Create an Index that is a combination of idx1 and idx2.
+    """
     is_trivial = idx1.is_trivial and idx2.is_trivial
     if isinstance(idx1, SimpleIndex) and isinstance(idx2, SimpleIndex):
         return SimpleIndex(is_trivial)
