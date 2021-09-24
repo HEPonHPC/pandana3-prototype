7d21885434aa421bd89eea49d92d34f8fd473a30
Marc Paterno <paterno@fnal.gov>
2021-09-24 11:32:10 -0500

    More unit testing of SimpleVar

--- pandana3/core/test/test_simple_var.py
+++ pandana3/core/test/test_simple_var.py
@@ -1,23 +1,27 @@
 import pytest
-from pandana3.core.var import SimpleVar, GroupedVar, Var, MutatedVar, FilteredVar
-from pandana3.core.cut import SimpleCut
-from pandana3.core.index import SimpleIndex
-from pandana3.core.grouping import Grouping
+from pandana3.core.var import SimpleVar
 import h5py as h5
 import pandas as pd
 import numpy as np
 
+@pytest.fixture()
+def sv00() -> SimpleVar:
+    return SimpleVar("electrons", ["pt", "eta"])
 
-def test_simple_var_multiple_table_names():
+def test_multiple_table_names():
     with pytest.raises(TypeError):
         _ = SimpleVar(["electrons", "muons"], ["pt"])
 
 
-def test_simple_var_column_names_not_list():
+def test_column_names_not_list():
     with pytest.raises(TypeError):
         _ = SimpleVar("electrons", "pt")
 
 
-def test_simple_var_column_names_empy_list():
+def test_column_names_empy_list():
     with pytest.raises(ValueError):
         _ = SimpleVar("electrons", [])
+
+
+def test_right_columns(sv00: SimpleVar) -> None:
+    assert sv00.columns == ["pt", "eta"]
